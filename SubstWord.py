8ea9c101bd1e616c2cbd670b049fdd6205c2bd76
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-07 14:14:53 +0300

    subWord function

---
+++ SubstWord.py
@@ -0,0 +1,13 @@
+import re
+
+def subWord(old: str, new: str, text: str) -> str:
+    """
+        Remplacer tous les occurence d'un mot par un autre mot dans un text
+        :param old: le mot à remplacer
+        :param new: le nouveau mot
+        :text: le text contenant le mot à remplacer
+        :return: un text modifié
+    """
+    pattern = r'\b'+old+r'\b'
+    print(pattern)
+    return re.sub(pattern, new, text)
