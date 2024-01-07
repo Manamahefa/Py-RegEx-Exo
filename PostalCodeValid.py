e495b0930fc748c62e97eb06386b0229187ff278
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-07 15:46:36 +0300

    postal code validation

--- PostalCodeValid.py
+++ PostalCodeValid.py
@@ -4,4 +4,4 @@ def isValidPostalCode(postal_code: str) -> bool:
     """
         Validation de code postal dans un format spécifique. ex: 12345 ou 12345-6789
     """
-    return re.match(r"\b\d{3,5}[-\d]*\b", postal_code)
+    return bool(re.match(r"\b\d{3,5}[-\d]*\b", postal_code))
