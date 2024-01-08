b3233446cff083fe43fd37b57d62885029b85f81
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 21:50:01 +0300

    add IGNORECASE to ip_pattern

--- ExtractIP.py
+++ ExtractIP.py
@@ -1,6 +1,6 @@
 import re
 
-ip_pattern = re.compile(r"\b(?:\d{1,3}\.){3}\d{1,3}\b|\b(?:[0-9a-z]{1,4}:){7}[0-9a-z]{1,4}\b")
+ip_pattern = re.compile(r"\b(?:\d{1,3}\.){3}\d{1,3}\b|\b(?:[0-9a-z]{1,4}:){7}[0-9a-z]{1,4}\b", re.I)
 
 def extractIP(text: str) -> list:
     """
