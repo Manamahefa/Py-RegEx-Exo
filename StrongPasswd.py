5969581bb5c8a795c592fc941fa3d538d5658bc3
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 21:11:04 +0300

    rectify strong passwork

--- StrongPasswd.py
+++ StrongPasswd.py
@@ -4,8 +4,11 @@ def isStrongPasswd(passwd = str) -> bool:
     """
         Verifie si un mot de passe est fort
     """
-    is_strong_passwd = len(passwd)>=8
-    patterns = ["[a-z]", "[A-Z]", "[0-9]","[è_çà=$ù!:;,#{}\[\]| +*-/&é\"'(-)^@<>]"]
-    for pattern in patterns:
-        is_strong_passwd &= bool(re.search(pattern, passwd))
-    return is_strong_passwd
+    # is_strong_passwd = len(passwd)>=8
+    # patterns = ["[a-z]", "[A-Z]", "[0-9]","[è_çà=$ù!:;,#{}\[\]| +*-/&é\"'(-)^@<>]"]
+    # for pattern in patterns:
+    #     is_strong_passwd &= bool(re.search(pattern, passwd))
+    # return is_strong_passwd
+
+    pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+    return bool(pattern.match(passwd))
