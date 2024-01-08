a65d531bcda1aff1caaabfcceaac2335d2cdc978
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 20:44:31 +0300

    rectify date pattern

--- DateValid.py
+++ DateValid.py
@@ -1,7 +1,7 @@
 import re
 
 #date au format "dd/mm/yyyy"
-date_pattern = re.compile(r"\b((0[1-9])|([1-2][0-9])|(3[0-1]))/((0[1-9])|(1[0-2]))/\d{4}\b")
+date_pattern = re.compile(r"\b((0[1-9])|([12][0-9])|(3[01]))/((0[1-9])|(1[0-2]))/\d{4}\b")
 
 def isValidDate(date: str, date_pattern = date_pattern) -> bool:
     """
