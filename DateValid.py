7fe1c6d590fb25b911b5d762813a929f1e34cefd
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 20:14:32 +0300

    clean code

--- DateValid.py
+++ DateValid.py
@@ -3,6 +3,8 @@ import re
 #date au format "dd/mm/yyyy"
 date_pattern = re.compile(r"\b((0[1-9])|([1-2][0-9])|(3[0-1]))/((0[1-9])|(1[0-2]))/\d{4}\b")
 
-def validDate(date: str) -> bool:
+def isValidDate(date: str, date_pattern = date_pattern) -> bool:
+    """
+        Verifie une date au format date_pattern
+    """
     return bool(date_pattern.match(date))
-
--- ExtractPhoneNbr.py
+++ ExtractPhoneNbr.py
@@ -1,13 +1,12 @@
 import re
 
-# phone_nbr_pattern = re.compile(r'(?:\+\d{1,3})?[()\s0-9-]{13,16}') # format conventionnel
-# phone_nbr_pattern = re.compile(r'\+?\d{1,3}?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}') # format conventionnel
-phone_nbr_pattern = re.compile(r"\+2613\d{8}|03\d{8}|03\d\s\d{2}\s\d{3}\s\d{2}|\+261\s3\d\s\d{2}\s\d{3}\s\d{2}") # num GASY
+# conventional_phone_pattern = re.compile(r'(?:\+\d{1,3})?[()\s0-9-]{13,16}') # format conventionnel
+# conventional_phone_pattern = re.compile(r'\+?\d{1,3}?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}') # format conventionnel
+gasy_phone_pattern = re.compile(r"\+2613\d{8}|03\d{8}|03\d\s\d{2}\s\d{3}\s\d{2}|\+261\s3\d\s\d{2}\s\d{3}\s\d{2}") # num GASY
 
-def extractPhoneNbr(text: str) -> list:
+def extractPhoneNbr(text: str, phone_pattern = gasy_phone_pattern) -> list:
     """
         EXTRACTION DES NUMEROS DE TELEPHONE D'UN TEXT
         :return: List des numéros de Téléphone trouvés
     """
-    result = phone_nbr_pattern.findall(text)
-    return result
+    return phone_pattern.findall(text)
