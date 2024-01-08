6e7786cf692fecea48bcb781d0f78362a7837d44
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 18:18:15 +0300

    extract @IP from a text

---
+++ ExtractIP.py
@@ -0,0 +1,10 @@
+import re
+
+#IPv4 et #IPv6
+ip_pattern = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b|\b[\da-f]{2}\.[\da-f]{2}\.[\da-f]{2}\.[\da-f]{2}\.[\da-f]{2}\.[\da-f]{2}\b")
+
+def extractIP(text: str) -> list:
+    """
+        Extraire toutes les addresses IP d'un texte
+    """
+    return ip_pattern.findall(text)
