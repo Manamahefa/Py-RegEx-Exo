f2170393e5a59eeb40f4c0adf76352ec39ef98b6
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 16:19:01 +0300

    date validation

--- DateValid.py
+++ DateValid.py
@@ -1,8 +1,8 @@
 import re
 
+#date au format "dd/mm/yyyy"
 date_pattern = re.compile(r"\b((0[1-9])|([1-2][0-9])|(3[0-1]))/((0[1-9])|(1[0-2]))/\d{4}\b")
 
 def validDate(date: str) -> bool:
-    print(date)
-    return date_pattern.match(date)
+    return bool(date_pattern.match(date))
 
