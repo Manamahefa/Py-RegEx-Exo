5a00e8d64479513cb38a0e6eaa78aa6ec0187301
Manamahefa <manamahefa.nyr@gmail.com>
2024-01-08 16:34:01 +0300

    rectify mail_pattern

--- MailValid.py
+++ MailValid.py
@@ -1,11 +1,14 @@
 import re
 
-mail_pattern = re.compile("[a-z0-9._-]+@[a-z0-9]+\.[a-z0-9]{2,3}", re.I)
+mail_pattern = re.compile(r"\b[a-z0-9._-]+@[a-z0-9]+\.[a-z0-9]{2,3}\b", re.I)
 
 def isValidMail(email: str) -> bool:
     """
-        Verifier si l'addresse e-mail a une format valid
+        Verifie le format d'une addresse e-mail
         :param email: l'addresse e-mail
         :return: True si l'addresse est valid sinon False
     """
-    return bool(mail_pattern.match(email))
+    print(email)
+    return mail_pattern.match(email)
+
+print(isValidMail("qsdf@el.com"))
